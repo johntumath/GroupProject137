7426f1c35ce956159202c591e0867fd73b3f8a37
johntumath <tumathjohn@gmail.com>
2018-02-20 11:29:34 -0800

    First Commit, blank project

---
+++ GroupProject137/GroupProject137/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroupProject137
+//
+//  Created by John Tumath on 2/20/18.
+//  Copyright © 2018 John Tumath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
