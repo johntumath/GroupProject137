bc87e020dc035f3081686d2072d13aed4399d9dd
AndrewKyu <andrewkyu95@gmail.com>
2018-04-01 14:57:28 -0700

    Created basic UI for the login page, imported FirebaseAuth

--- GroupProject137/GroupProject137/ViewController.swift
+++ GroupProject137/GroupProject137/ViewController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInSelector: UISegmentedControl!
+    
+    @IBOutlet weak var signInLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var isSignIn: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signInSelectorChanged(_ sender: UISegmentedControl) {
+        
+        //Flipping the boolean
+        isSignIn = !isSignIn
+        
+        //Check the bool and set the button and labels
+        if isSignIn{
+            signInLabel.text = "Sign In"
+            loginButton.setTitle("Sign In", for: .normal)
+        }else{
+            signInLabel.text = "Register"
+            loginButton.setTitle("Register", for: .normal)
+        }
+        
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        //Check if it's sign in or register
+        if isSignIn{
+            //Sign in the user with FireBase
+        }else{
+            //Register the user with FireBase
+        }
+    }
 
 }
 
